abe255110f99fc4980359fc196ac9235d0c05ed4
Manyari <Iraynam.1768@gmail.com>
2020-11-13 18:23:57 -0600

    First commit

--- src/app/components/Admin/Admin.tsx
+++ src/app/components/Admin/Admin.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './style.css';
 
 const Admin = () => {
    return (
       <>
-      Dashboard
+      <h1     
+       className ="title">Legislación Mexicana</h1>
+      <input 
+      className ="form-control"  
+      style={{backgroundColor:"#e8e9e823",
+      width:"50%",marginLeft:"24%", 
+      marginTop:"2%"}}
+      /><i className="fas fa-search"
+      style={{backgroundColor:"#e8e9e823",
+      marginLeft:"48%", 
+      marginTop:"2%"}}></i>
       </>
    )
 }
