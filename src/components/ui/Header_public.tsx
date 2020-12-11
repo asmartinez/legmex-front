dae2b010c6f4fb4944a46c0dcbe3b8daf4ee78e5
Manyari <Iraynam.1768@gmail.com>
2020-12-10 20:48:58 -0600

    All Changes

---
+++ src/components/ui/Header_public.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { Navbar, Nav, Container } from 'reactstrap';
+import DropdownProfile from './Dropdown_public';
+import logo from '../../assets/img/logooficial.svg';
+import { useWindowSize } from 'react-use';
+
+const Header = () => {
+   const [toggle, setToggle] = useState<boolean>(false);
+
+   const { width } = useWindowSize();
+
+   const displayNone = width < 768;
+
+   const showSidebar = () => {
+      toggle ? setToggle(false) : setToggle(true);   
+   }
+
+   useEffect( () => {
+      const nav = document.getElementById('navbar');
+      const backdrop = document.getElementById('backdrop');
+      const backdropActive = document.querySelector('.backdrop');
+
+      if (toggle) {
+         backdrop?.classList.add('backdrop');
+         nav?.classList.add('show-sidebar');
+      }
+
+      backdropActive?.addEventListener('click', function() {
+         nav?.classList.remove('show-sidebar');
+         backdrop?.classList.remove('backdrop');
+      })
+
+      return () => {
+         setToggle(false);
+      }
+   }, [toggle]);
+
+   return (
+      <>
+         {/* Desktop and Mobile Header */}
+         <Navbar className={ displayNone ? 'navbar-vertical fixed-left navbar-light bg-white' : 'navbar-top' }  expand="md" id="navbar-main">
+            <Container fluid> 
+               { !displayNone && <span className="h4 mb-0 text-uppercase d-none d-lg-inline-block"> {/*Dashboard*/} </span> }
+               { displayNone && <img className="navbar-brand-img" src={logo} alt="..."/> }
+               <Nav className={`align-items-center ${displayNone ? 'd-md-flex' : ''}`} navbar={!displayNone}>
+                  <DropdownProfile />
+               </Nav>
+            </Container>
+         </Navbar>
+      </>
+   );
+}
+
+export default Header;
