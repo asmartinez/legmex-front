df68ecdd98e71a0189591dc359c1cd5c0ed7480e
Manyari <Iraynam.1768@gmail.com>
2020-12-10 20:48:44 -0600

    All Changes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { RouteCustom } from "./shared/utils/interfaces";
 import Dashboard from './components/pages/admin_views/Dashboard';
 import Users from './components/pages/admin_views/Users';
 import Inicio from "./components/pages/public_views/Inicio";
+import Panel from './components/pages/admin_views/Files'; 
 
 const routes: Array<RouteCustom> = [
    {
@@ -11,7 +12,7 @@ const routes: Array<RouteCustom> = [
       layout: '/public',
       routerLink: '/inicio',
       component: Inicio
-   },
+   }, 
    {
       label: 'Dashboard',
       iconType: 'bx',
@@ -28,6 +29,14 @@ const routes: Array<RouteCustom> = [
       routerLink: '/users',
       component: Users
    }, 
+   {
+      label: 'Registro',
+      iconType: 'bx',
+      iconName: 'users',
+      layout: '/admin',
+      routerLink: '/panel',
+      component: Panel
+   }
 ];
 
 export default routes;
