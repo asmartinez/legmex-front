e564084d83cb1f30ec9433e8968e37dd3ae60f3b
RChinCaamal <raulchincaamal@outlook.com>
2021-03-06 18:52:22 -0600

    #10 única carga del archivo

--- src/components/ui/common/pdf-viewer/PDFViewer.tsx
+++ src/components/ui/common/pdf-viewer/PDFViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import Loader from 'components/ui/common/Loader';
 import ControlViewer from 'components/ui/common/pdf-viewer/ControlViewer';
@@ -9,6 +9,7 @@ const PDFViewer = (item: IPDFViewer) => {
    const [numPages, setNumPages] = useState(0);
    const [pageNumber, setPageNumber] = useState(1);
    const [scale, setScale] = useState(1.0);
+   const [file, setFile] = useState<Blob>();
    const [isLoading, setIsLoading] = useState<boolean>(true);
 
    const onDocumentLoadSuccess = ({ numPages }: any) => {
@@ -16,20 +17,35 @@ const PDFViewer = (item: IPDFViewer) => {
       setIsLoading(false);
    }
 
+   useEffect(() => {
+      fetch(documentService.getDocumentPDF(item.path))
+      .then(response => response.blob())
+      .then(blob => {
+         setFile(blob)
+         setIsLoading(false);
+      });
+   }, [item.path])
+
    return (
       <div className="d-flex flex-column align-items-center w-100" style={{marginTop: '10px'}}>
-         {/* isLoading && <Loader /> */}
-         <Document
-          file={{ url: documentService.getDocumentPDF(item.path), mode: 'no-cors'}}
-          onLoadSuccess={onDocumentLoadSuccess} >
-            <Page pageNumber={pageNumber} scale={scale} width={500}/>
-         </Document>
-         <ControlViewer
-          scale={scale}
-          pageNumber={pageNumber}
-          numberPages={numPages}
-          setPageNumber={setPageNumber}
-          setScale={setScale}/>
+         {
+            !isLoading &&
+            (
+               <>
+                  <Document
+                   file={file}
+                   onLoadSuccess={onDocumentLoadSuccess} >
+                     <Page pageNumber={pageNumber} scale={scale} width={500}/>
+                  </Document>
+                  <ControlViewer
+                   scale={scale}
+                   pageNumber={pageNumber}
+                   numberPages={numPages}
+                   setPageNumber={setPageNumber}
+                   setScale={setScale}/>
+               </>
+            )
+         }
       </div>
    )
 }
